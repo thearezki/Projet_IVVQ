f70569e7e7539e157561ed8b3c55fd0e91fc7034
thearezki <arezki0927@gmail.com>
2026-01-27 19:23:51 +0100

    Feat: Ajout de la détection des logs CRITICAL

--- auditor.py
+++ auditor.py
@@ -6,12 +6,12 @@ def analyze_logs(filepath):
 
     with open(filepath, 'r') as f:
         for line in f:
-            if "ERROR" in line:
+            if "ERROR" in line or "CRITICAL" in line:
 
                 count = count + 1
                 print(f"Alerte détectée : {line.strip()}")
     
-    print(f"Analyse terminée. Nombre d'erreurs trouvées : {count}")
+    print(f"Analyse terminée. Nombre d'erreurs ou d'alertes trouvées : {count}")
 
 if __name__ == "__main__":
     analyze_logs("server.log")
