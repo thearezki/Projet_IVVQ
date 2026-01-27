5fbd0979399e4155bd2e7f460b4146faea5acf25
thearezki <arezki0927@gmail.com>
2026-01-27 19:20:17 +0100

    Feat: Ajout de la détection des logs ERROR

--- auditor.py
+++ auditor.py
@@ -1,10 +1,17 @@
 def analyze_logs(filepath):
     print(f"--- Analyse du fichier : {filepath} ---")
     
+    
+    count = 0
+
     with open(filepath, 'r') as f:
-        
-        for line in f: 
-            print(line.strip())
+        for line in f:
+            if "ERROR" in line:
+
+                count = count + 1
+                print(f"Alerte détectée : {line.strip()}")
+    
+    print(f"Analyse terminée. Nombre d'erreurs trouvées : {count}")
 
 if __name__ == "__main__":
     analyze_logs("server.log")
