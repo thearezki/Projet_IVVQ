9c2160feacfe4a0e75479e08377f3c861959fb60
thearezki <arezki0927@gmail.com>
2026-01-27 19:34:08 +0100

    : La fonction retourne le nombre d'erreurs au lieu de l'afficher

--- auditor.py
+++ auditor.py
@@ -1,17 +1,18 @@
 def analyze_logs(filepath):
     print(f"--- Analyse du fichier : {filepath} ---")
     
-    
     count = 0
 
     with open(filepath, 'r') as f:
         for line in f:
             if "ERROR" in line or "CRITICAL" in line:
-
                 count = count + 1
                 print(f"Alerte détectée : {line.strip()}")
-    
-    print(f"Analyse terminée. Nombre d'erreurs ou d'alertes trouvées : {count}")
+
+    return count
 
 if __name__ == "__main__":
-    analyze_logs("server.log")
+
+    nombre_erreurs = analyze_logs("server.log")
+
+    print(f"Analyse terminée. Nombre d'erreurs trouvées : {nombre_erreurs}")
