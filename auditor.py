700ef0b721a44b978c4c2b130e6f2a3745a2104e
thearezki <arezki0927@gmail.com>
2026-01-31 23:31:00 +0100

    ajout de l'archivage du fichier server.log dans un dossier

--- auditor.py
+++ auditor.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 def analyze_logs(filepath):
     print(f"--- Analyse du fichier : {filepath} ---")
     
@@ -17,5 +20,23 @@ def analyze_logs(filepath):
         return 0 
 
     return count
+
+
+def move_to_archive(filepath):
+    
+    archive_dir = "archives"
+    
+    if not os.path.exists(archive_dir):
+        os.makedirs(archive_dir)
+    
+    
+    print(f" Déplacement de {filepath} vers {archive_dir}...")
+    shutil.move(filepath, archive_dir)
+
 if __name__ == "__main__":
-    analyze_logs("server.log")
+    nombre_erreurs= analyze_logs("server.log")
+
+    print(f"Analyse terminée. Nombre d'erreurs trouvées : {nombre_erreurs}")
+
+    move_to_archive("server.log")
+	
